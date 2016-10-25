91a288dc7e31c889ffbee4537359e0acb33a56fd
Jason Conrad <jdconrad@liberty.edu>
2016-10-25 08:00:50 -0600

    updated bottles

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,24 +3,30 @@ require 'pry'
 class Bottles
   attr_reader :number_of_bottles
 
-  def initialize(bottles)
-    @number_of_bottles = bottles
-  end
-
   def sing_the_song(bottles)
-    until @number_of_bottles == 1
-      puts line
-      count
-    end
+    @number_of_bottles = bottles
+    until  number_of_bottles == 1
+      if number_of_bottles > 2
+        puts line
+        count_bottles_left
+      else
+        puts second_to_last_line
+        count_bottles_left
+      end
     puts last_line_of_the_song
+    end
   end
 
   def line
-     "#{@number_of_bottles} bottles of beer on the wall, #{@number_of_bottles} bottles of beer,\n take one down pass it around #{@number_of_bottles - 1} bottles of beer"
+    "#{@number_of_bottles} bottles of beer on the wall, #{@number_of_bottles} bottles of beer,\n take one down pass it around #{@number_of_bottles - 1} bottles of beer"
   end
 
-  def count
+  def count_bottles_left
     @number_of_bottles -= 1
+  end
+
+  def second_to_last_line
+    "2 bottles of beer on the wall, 2 bottles of beer,\n take one down pass it around 1 bottle of beer"
   end
 
   def last_line_of_the_song
@@ -31,5 +37,5 @@ Go to the store and buy some more, 99 bottles of beer on the wall."
   end
 end
 
-b = Bottles.new(3)
-b.sing_the_song(3)
+bottles = Bottles.new
+bottles.sing_the_song(3)
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/bottles'
@@ -5,32 +7,31 @@ require './lib/bottles'
 class BottlesTest < Minitest::Test
 
   def test_bottles_class_exists
-    assert_instance_of Bottles, Bottles.new(2)
+    assert_instance_of Bottles, Bottles.new
   end
 
-
-  def test_it_can_take_in_number_of_bottles
-    b = Bottles.new(2)
-
-    assert_equal 2, b.number_of_bottles
+  def test_it_keeps_track_of_the_count_through_a_song
+    b = Bottles.new
+    b.sing_the_song(7)
+    assert_equal 0, b.count_bottles_left
   end
 
-  def test_it_can_print_the_number_of_bottles_in_a_line_of_the_song
-    b = Bottles.new(7)
+  def test_it_can_take_in_a_number
+    b= Bottles.new
 
-    result = "7 bottles of beer on the wall, 7 bottles of beer,\n take one down pass it around 6 bottles of beer"
+    b.sing_the_song(2)
 
-    assert_equal result, b.line
-  end
+    assert Fixnum, 
 
-  def test_it_keeps_track_of_the_count
-    b = Bottles.new(7)
+  def test_it_will_print_the_second_to_last_line_of_the_song
+    b = Bottles.new
 
-    assert_equal 6, b.count
+    result = "2 bottles of beer on the wall, 2 bottles of beer,\n take one down pass it around 1 bottle of beer"
+    assert_equal result, b.second_to_last_line
   end
 
   def test_it_will_print_the_last_line_of_the_song
-    b = Bottles.new(1)
+    b = Bottles.new
 
     result = "1 bottle of beer on the wall, 1 bottle of beer.
 Take one down and pass it around, no more bottles of beer on the wall.
@@ -39,23 +40,4 @@ Go to the store and buy some more, 99 bottles of beer on the wall."
 
     assert_equal result, b.last_line_of_the_song
   end
-
-  def test_it_sings_the_song
-    b = Bottles.new(2)
-
-    result = " "
-
-    assert_equal result, b.sing_the_song(2)
-  end
-
-#   def test_it_will_print_the_last_line_of_the_song_if_the_number_of_bottles_is_one
-#     b = Bottles.new(1)
-#
-#     result = "1 bottle of beer on the wall, 1 bottle of beer.
-# Take one down and pass it around, no more bottles of beer on the wall.
-# No more bottles of beer on the wall, no more bottles of beer.
-# Go to the store and buy some more, 99 bottles of beer on the wall."
-#
-#   assert_equal result, b.sing_the_song(1)
-#   end
 end
